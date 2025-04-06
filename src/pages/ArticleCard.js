413e55748e7b8614b616759f6dab94d53c96c873
Dmitry Akimenko <molotovsteam@mail.ru>
2025-04-07 02:19:32 +0300

    sadddd

--- src/pages/ArticleCard.js
+++ src/pages/ArticleCard.js
@@ -1,14 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 import "./ArticleCard.scss";
 import Vector from "./Vector.svg";
+import VectorLiked from "./Heart_corazón 1.svg";
 
 const ArticleCard = ({ article }) => {
+  const [favoritesCount, setFavoritesCount] = useState(article.favoritesCount);
+  const [favorited, setFavorited] = useState(article.favorited);
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const storedToken = localStorage.getItem("token");
+    if (storedToken) setToken(storedToken);
+  }, []);
+
   function formatDate(dateString) {
     const date = new Date(dateString);
     const options = { year: "numeric", month: "long", day: "numeric" };
     return date.toLocaleDateString("en-US", options);
   }
+
+  const handleLikeClick = async () => {
+    if (!token) return; // Только для авторизованных пользователей
+
+    try {
+      if (favorited) {
+        await axios.delete(
+          `https://blog-platform.kata.academy/api/articles/${article.slug}/favorite`,
+          {
+            headers: { Authorization: `Token ${token}` },
+          },
+        );
+        setFavoritesCount((prev) => prev - 1);
+      } else {
+        await axios.post(
+          `https://blog-platform.kata.academy/api/articles/${article.slug}/favorite`,
+          {},
+          {
+            headers: { Authorization: `Token ${token}` },
+          },
+        );
+        setFavoritesCount((prev) => prev + 1);
+      }
+      setFavorited(!favorited);
+    } catch (error) {
+      console.error("Ошибка при попытке лайкнуть статью:", error);
+    }
+  };
 
   return (
     <div className="article-card">
@@ -18,8 +57,14 @@ const ArticleCard = ({ article }) => {
             <h2>
               <Link to={`/articles/${article.slug}`}>{article.title}</Link>
             </h2>
-            <img src={Vector} alt="" />
-            <p className="article-slugcount-likes">{article.favoritesCount}</p>
+            <img
+              src={favorited ? VectorLiked : Vector}
+              alt="Like"
+              className={`like-icon ${favorited ? "liked" : ""}`}
+              onClick={handleLikeClick}
+              style={{ cursor: token ? "pointer" : "default" }}
+            />
+            <p className="article-slugcount-likes">{favoritesCount}</p>
           </div>
 
           <p>
